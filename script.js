9e7464f79a3161a287cf81738b05da303baf8137
xskp <124841396+xskp@users.noreply.github.com>
2026-09-04 13:30:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 /*
 ==================================================
 LOSOWANIE ROZDZIAŁU BIBLII
-Biblia Wujka
+Biblia Wujka - domena publiczna (1599)
 ==================================================
 */
-
 
 /*
 ==================================================
@@ -13,493 +12,80 @@ LISTA KSIĄG
 */
 
 const books = [
-
-    /* =========================
-       STARY TESTAMENT
-       ========================= */
-
-    {
-        name: "Księga Rodzaju",
-        testament: "ST",
-        chapters: 50
-    },
-
-    {
-        name: "Księga Wyjścia",
-        testament: "ST",
-        chapters: 40
-    },
-
-    {
-        name: "Księga Kapłańska",
-        testament: "ST",
-        chapters: 27
-    },
-
-    {
-        name: "Księga Liczb",
-        testament: "ST",
-        chapters: 36
-    },
-
-    {
-        name: "Księga Powtórzonego Prawa",
-        testament: "ST",
-        chapters: 34
-    },
-
-    {
-        name: "Księga Jozuego",
-        testament: "ST",
-        chapters: 24
-    },
-
-    {
-        name: "Księga Sędziów",
-        testament: "ST",
-        chapters: 21
-    },
-
-    {
-        name: "Księga Rut",
-        testament: "ST",
-        chapters: 4
-    },
-
-    {
-        name: "1 Księga Samuela",
-        testament: "ST",
-        chapters: 31
-    },
-
-    {
-        name: "2 Księga Samuela",
-        testament: "ST",
-        chapters: 24
-    },
-
-    {
-        name: "1 Księga Królewska",
-        testament: "ST",
-        chapters: 22
-    },
-
-    {
-        name: "2 Księga Królewska",
-        testament: "ST",
-        chapters: 25
-    },
-
-    {
-        name: "1 Księga Kronik",
-        testament: "ST",
-        chapters: 29
-    },
-
-    {
-        name: "2 Księga Kronik",
-        testament: "ST",
-        chapters: 36
-    },
-
-    {
-        name: "Księga Ezdrasza",
-        testament: "ST",
-        chapters: 10
-    },
-
-    {
-        name: "Księga Nehemiasza",
-        testament: "ST",
-        chapters: 13
-    },
-
-    {
-        name: "Księga Tobiasza",
-        testament: "ST",
-        chapters: 14
-    },
-
-    {
-        name: "Księga Judyty",
-        testament: "ST",
-        chapters: 16
-    },
-
-    {
-        name: "Księga Estery",
-        testament: "ST",
-        chapters: 16
-    },
-
-    {
-        name: "1 Księga Machabejska",
-        testament: "ST",
-        chapters: 16
-    },
-
-    {
-        name: "2 Księga Machabejska",
-        testament: "ST",
-        chapters: 15
-    },
-
-    {
-        name: "Księga Hioba",
-        testament: "ST",
-        chapters: 42
-    },
-
-    {
-        name: "Księga Psalmów",
-        testament: "ST",
-        chapters: 150
-    },
-
-    {
-        name: "Księga Przysłów",
-        testament: "ST",
-        chapters: 31
-    },
-
-    {
-        name: "Księga Koheleta",
-        testament: "ST",
-        chapters: 12
-    },
-
-    {
-        name: "Pieśń nad Pieśniami",
-        testament: "ST",
-        chapters: 8
-    },
-
-    {
-        name: "Księga Mądrości",
-        testament: "ST",
-        chapters: 19
-    },
-
-    {
-        name: "Mądrość Syracha",
-        testament: "ST",
-        chapters: 51
-    },
-
-    {
-        name: "Księga Izajasza",
-        testament: "ST",
-        chapters: 66
-    },
-
-    {
-        name: "Księga Jeremiasza",
-        testament: "ST",
-        chapters: 52
-    },
-
-    {
-        name: "Lamentacje Jeremiasza",
-        testament: "ST",
-        chapters: 5
-    },
-
-    {
-        name: "Księga Barucha",
-        testament: "ST",
-        chapters: 6
-    },
-
-    {
-        name: "Księga Ezechiela",
-        testament: "ST",
-        chapters: 48
-    },
-
-    {
-        name: "Księga Daniela",
-        testament: "ST",
-        chapters: 14
-    },
-
-    {
-        name: "Księga Ozeasza",
-        testament: "ST",
-        chapters: 14
-    },
-
-    {
-        name: "Księga Joela",
-        testament: "ST",
-        chapters: 4
-    },
-
-    {
-        name: "Księga Amosa",
-        testament: "ST",
-        chapters: 9
-    },
-
-    {
-        name: "Księga Abdiasza",
-        testament: "ST",
-        chapters: 1
-    },
-
-    {
-        name: "Księga Jonasza",
-        testament: "ST",
-        chapters: 4
-    },
-
-    {
-        name: "Księga Micheasza",
-        testament: "ST",
-        chapters: 7
-    },
-
-    {
-        name: "Księga Nahuma",
-        testament: "ST",
-        chapters: 3
-    },
-
-    {
-        name: "Księga Habakuka",
-        testament: "ST",
-        chapters: 3
-    },
-
-    {
-        name: "Księga Sofoniasza",
-        testament: "ST",
-        chapters: 3
-    },
-
-    {
-        name: "Księga Aggeusza",
-        testament: "ST",
-        chapters: 2
-    },
-
-    {
-        name: "Księga Zachariasza",
-        testament: "ST",
-        chapters: 14
-    },
-
-    {
-        name: "Księga Malachiasza",
-        testament: "ST",
-        chapters: 3
-    },
-
-
-    /* =========================
-       NOWY TESTAMENT
-       ========================= */
-
-    {
-        name: "Ewangelia Mateusza",
-        testament: "NT",
-        chapters: 28
-    },
-
-    {
-        name: "Ewangelia Marka",
-        testament: "NT",
-        chapters: 16
-    },
-
-    {
-        name: "Ewangelia Łukasza",
-        testament: "NT",
-        chapters: 24
-    },
-
-    {
-        name: "Ewangelia Jana",
-        testament: "NT",
-        chapters: 21
-    },
-
-    {
-        name: "Dzieje Apostolskie",
-        testament: "NT",
-        chapters: 28
-    },
-
-    {
-        name: "List do Rzymian",
-        testament: "NT",
-        chapters: 16
-    },
-
-    {
-        name: "1 List do Koryntian",
-        testament: "NT",
-        chapters: 16
-    },
-
-    {
-        name: "2 List do Koryntian",
-        testament: "NT",
-        chapters: 13
-    },
-
-    {
-        name: "List do Galatów",
-        testament: "NT",
-        chapters: 6
-    },
-
-    {
-        name: "List do Efezjan",
-        testament: "NT",
-        chapters: 6
-    },
-
-    {
-        name: "List do Filipian",
-        testament: "NT",
-        chapters: 4
-    },
-
-    {
-        name: "List do Kolosan",
-        testament: "NT",
-        chapters: 4
-    },
-
-    {
-        name: "1 List do Tesaloniczan",
-        testament: "NT",
-        chapters: 5
-    },
-
-    {
-        name: "2 List do Tesaloniczan",
-        testament: "NT",
-        chapters: 3
-    },
-
-    {
-        name: "1 List do Tymoteusza",
-        testament: "NT",
-        chapters: 6
-    },
-
-    {
-        name: "2 List do Tymoteusza",
-        testament: "NT",
-        chapters: 4
-    },
-
-    {
-        name: "List do Tytusa",
-        testament: "NT",
-        chapters: 3
-    },
-
-    {
-        name: "List do Filemona",
-        testament: "NT",
-        chapters: 1
-    },
-
-    {
-        name: "List do Hebrajczyków",
-        testament: "NT",
-        chapters: 13
-    },
-
-    {
-        name: "List Jakuba",
-        testament: "NT",
-        chapters: 5
-    },
-
-    {
-        name: "1 List Piotra",
-        testament: "NT",
-        chapters: 5
-    },
-
-    {
-        name: "2 List Piotra",
-        testament: "NT",
-        chapters: 3
-    },
-
-    {
-        name: "1 List Jana",
-        testament: "NT",
-        chapters: 5
-    },
-
-    {
-        name: "2 List Jana",
-        testament: "NT",
-        chapters: 1
-    },
-
-    {
-        name: "3 List Jana",
-        testament: "NT",
-        chapters: 1
-    },
-
-    {
-        name: "List Judy",
-        testament: "NT",
-        chapters: 1
-    },
-
-    {
-        name: "Apokalipsa św. Jana",
-        testament: "NT",
-        chapters: 22
-    }
-
+    { name: "Księga Rodzaju", testament: "ST", chapters: 50 },
+    { name: "Księga Wyjścia", testament: "ST", chapters: 40 },
+    { name: "Księga Kapłańska", testament: "ST", chapters: 27 },
+    { name: "Księga Liczb", testament: "ST", chapters: 36 },
+    { name: "Księga Powtórzonego Prawa", testament: "ST", chapters: 34 },
+    { name: "Księga Jozuego", testament: "ST", chapters: 24 },
+    { name: "Księga Sędziów", testament: "ST", chapters: 21 },
+    { name: "Księga Rut", testament: "ST", chapters: 4 },
+    { name: "1 Księga Samuela", testament: "ST", chapters: 31 },
+    { name: "2 Księga Samuela", testament: "ST", chapters: 24 },
+    { name: "1 Księga Królewska", testament: "ST", chapters: 22 },
+    { name: "2 Księga Królewska", testament: "ST", chapters: 25 },
+    { name: "1 Księga Kronik", testament: "ST", chapters: 29 },
+    { name: "2 Księga Kronik", testament: "ST", chapters: 36 },
+    { name: "Księga Ezdrasza", testament: "ST", chapters: 10 },
+    { name: "Księga Nehemiasza", testament: "ST", chapters: 13 },
+    { name: "Księga Tobiasza", testament: "ST", chapters: 14 },
+    { name: "Księga Judyty", testament: "ST", chapters: 16 },
+    { name: "Księga Estery", testament: "ST", chapters: 16 },
+    { name: "1 Księga Machabejska", testament: "ST", chapters: 16 },
+    { name: "2 Księga Machabejska", testament: "ST", chapters: 15 },
+    { name: "Księga Hioba", testament: "ST", chapters: 42 },
+    { name: "Księga Psalmów", testament: "ST", chapters: 150 },
+    { name: "Księga Przysłów", testament: "ST", chapters: 31 },
+    { name: "Księga Koheleta", testament: "ST", chapters: 12 },
+    { name: "Pieśń nad Pieśniami", testament: "ST", chapters: 8 },
+    { name: "Księga Mądrości", testament: "ST", chapters: 19 },
+    { name: "Mądrość Syracha", testament: "ST", chapters: 51 },
+    { name: "Księga Izajasza", testament: "ST", chapters: 66 },
+    { name: "Księga Jeremiasza", testament: "ST", chapters: 52 },
+    { name: "Lamentacje Jeremiasza", testament: "ST", chapters: 5 },
+    { name: "Księga Barucha", testament: "ST", chapters: 6 },
+    { name: "Księga Ezechiela", testament: "ST", chapters: 48 },
+    { name: "Księga Daniela", testament: "ST", chapters: 14 },
+    { name: "Księga Ozeasza", testament: "ST", chapters: 14 },
+    { name: "Księga Joela", testament: "ST", chapters: 4 },
+    { name: "Księga Amosa", testament: "ST", chapters: 9 },
+    { name: "Księga Abdiasza", testament: "ST", chapters: 1 },
+    { name: "Księga Jonasza", testament: "ST", chapters: 4 },
+    { name: "Księga Micheasza", testament: "ST", chapters: 7 },
+    { name: "Księga Nahuma", testament: "ST", chapters: 3 },
+    { name: "Księga Habakuka", testament: "ST", chapters: 3 },
+    { name: "Księga Sofoniasza", testament: "ST", chapters: 3 },
+    { name: "Księga Aggeusza", testament: "ST", chapters: 2 },
+    { name: "Księga Zachariasza", testament: "ST", chapters: 14 },
+    { name: "Księga Malachiasza", testament: "ST", chapters: 3 },
+    { name: "Ewangelia Mateusza", testament: "NT", chapters: 28 },
+    { name: "Ewangelia Marka", testament: "NT", chapters: 16 },
+    { name: "Ewangelia Łukasza", testament: "NT", chapters: 24 },
+    { name: "Ewangelia Jana", testament: "NT", chapters: 21 },
+    { name: "Dzieje Apostolskie", testament: "NT", chapters: 28 },
+    { name: "List do Rzymian", testament: "NT", chapters: 16 },
+    { name: "1 List do Koryntian", testament: "NT", chapters: 16 },
+    { name: "2 List do Koryntian", testament: "NT", chapters: 13 },
+    { name: "List do Galatów", testament: "NT", chapters: 6 },
+    { name: "List do Efezjan", testament: "NT", chapters: 6 },
+    { name: "List do Filipian", testament: "NT", chapters: 4 },
+    { name: "List do Kolosan", testament: "NT", chapters: 4 },
+    { name: "1 List do Tesaloniczan", testament: "NT", chapters: 5 },
+    { name: "2 List do Tesaloniczan", testament: "NT", chapters: 3 },
+    { name: "1 List do Tymoteusza", testament: "NT", chapters: 6 },
+    { name: "2 List do Tymoteusza", testament: "NT", chapters: 4 },
+    { name: "List do Tytusa", testament: "NT", chapters: 3 },
+    { name: "List do Filemona", testament: "NT", chapters: 1 },
+    { name: "List do Hebrajczyków", testament: "NT", chapters: 13 },
+    { name: "List Jakuba", testament: "NT", chapters: 5 },
+    { name: "1 List Piotra", testament: "NT", chapters: 5 },
+    { name: "2 List Piotra", testament: "NT", chapters: 3 },
+    { name: "1 List Jana", testament: "NT", chapters: 5 },
+    { name: "2 List Jana", testament: "NT", chapters: 1 },
+    { name: "3 List Jana", testament: "NT", chapters: 1 },
+    { name: "List Judy", testament: "NT", chapters: 1 },
+    { name: "Apokalipsa św. Jana", testament: "NT", chapters: 22 }
 ];
-
-
-/*
-==================================================
-PRZYKŁADOWE TEKSTY ROZDZIAŁÓW (DO TESTOWANIA)
-==================================================
-*/
-
-const sampleTexts = {
-    "Księga Rodzaju": {
-        1: [
-            "Na początku Bóg stworzył niebo i ziemię.",
-            "A ziemia była pusta i próżna, i ciemności były nad głębokością, a Duch Boży unaszał się nad wodami.",
-            "I rzekł Bóg: Niech się stanie światłość. I stała się światłość.",
-            "I widział Bóg światłość, że była dobra, i oddzielił światłość od ciemności.",
-            "I nazwał Bóg światłość dniem, a ciemność nocą. I był wieczór i poranek, dzień pierwszy."
-        ]
-    },
-    "Ewangelia Jana": {
-        1: [
-            "Na początku było Słowo, a Słowo było u Boga, a Bogiem było Słowo.",
-            "To było na początku u Boga.",
-            "Wszystko przez Nie się stało, a bez Niego nic się nie stało, co się stało.",
-            "W Nim było życie, a życie było światłością ludzi.",
-            "A światłość w ciemnościach świeci, a ciemności jej nie ogarnęły."
-        ]
-    },
-    "Księga Psalmów": {
-        23: [
-            "Pan jest pasterzem moim, niczego mi nie braknie.",
-            "Na miejscu zielonym tam mię posadził, nad wodą posilenia wychował mię.",
-            "Duszę moją nawrócił, prowadził mię ścieżkami sprawiedliwości dla imienia swego.",
-            "Bo choćbym chodził w pośród cienia śmierci, nie będę się bał złego, bo ty jesteś ze mną.",
-            "Laska twoja i kij twój, te mię pocieszyły."
-        ]
-    }
-};
-
 
 /*
 ==================================================
@@ -508,13 +94,10 @@ ZMIENNE
 */
 
 let selectedTestament = "ALL";
-
 let pool = [];
-
 let timer = null;
-
 let currentChapter = null;
-
+let bibleData = null; // Tutaj załadujemy tekst Biblii
 
 /*
 ==================================================
@@ -533,6 +116,23 @@ const chapterTextTitle = document.getElementById("chapterTextTitle");
 const chapterTextContent = document.getElementById("chapterTextContent");
 const closeChapterButton = document.getElementById("closeChapter");
 
+/*
+==================================================
+ŁADOWANIE TEKSTU BIBLII
+==================================================
+*/
+
+async function loadBible() {
+    try {
+        const response = await fetch('biblia-wujka.json');
+        bibleData = await response.json();
+        console.log('✅ Biblia Wujka załadowana!');
+        console.log('📚 Dostępne księgi:', Object.keys(bibleData).length);
+    } catch (error) {
+        console.error('❌ Błąd ładowania Biblii:', error);
+        console.log('ℹ️ Upewnij się, że plik biblia-wujka.json znajduje się obok index.html');
+    }
+}
 
 /*
 ==================================================
@@ -562,7 +162,6 @@ function createPool() {
     });
 }
 
-
 /*
 ==================================================
 LOSOWANIE JEDNEGO ROZDZIAŁU
@@ -574,7 +173,6 @@ function randomChapter() {
     return pool[randomIndex];
 }
 
-
 /*
 ==================================================
 FORMATOWANIE NAZWY
@@ -584,7 +182,6 @@ FORMATOWANIE NAZWY
 function chapterLabel(chapter) {
     return chapter.name + " — rozdział " + chapter.chapter;
 }
-
 
 /*
 ==================================================
@@ -624,7 +221,6 @@ choiceButtons.forEach(button => {
         }
     });
 });
-
 
 /*
 ==================================================
@@ -668,7 +264,6 @@ startButton.addEventListener("click", function() {
     animation();
 });
 
-
 /*
 ==================================================
 STOP
@@ -678,7 +273,6 @@ STOP
 stopButton.addEventListener("click", function() {
     finish();
 });
-
 
 /*
 ==================================================
@@ -721,7 +315,6 @@ function finish() {
     stopButton.disabled = true;
 }
 
-
 /*
 ==================================================
 WYŚWIETLANIE TEKSTU ROZDZIAŁU
@@ -729,25 +322,13 @@ WYŚWIETLANIE TEKSTU ROZDZIAŁU
 */
 
 function displayChapterText(chapter) {
-    let verses = null;
-
-    // Sprawdź czy mamy przykładowy tekst
-    if (sampleTexts[chapter.name] && sampleTexts[chapter.name][chapter.chapter]) {
-        verses = sampleTexts[chapter.name][chapter.chapter];
-    }
-
-    if (!verses) {
+    // Sprawdź czy Biblia jest załadowana
+    if (!bibleData) {
         chapterTextTitle.textContent = chapterLabel(chapter);
         chapterTextContent.innerHTML = `
             <div style="text-align: center; padding: 20px; color: #766f64;">
-                <p>Przepraszamy, pełny tekst tego rozdziału nie jest jeszcze dostępny.</p>
-                <p style="font-size: 14px;">Pracujemy nad dodaniem wszystkich tekstów.</p>
-                <p style="font-size: 14px;">Na razie możesz przetestować:</p>
-                <p style="font-size: 14px; margin-top: 10px;">
-                    • Księga Rodzaju, rozdział 1<br>
-                    • Ewangelia Jana, rozdział 1<br>
-                    • Księga Psalmów, rozdział 23
-                </p>
+                <p>⏳ Ładowanie tekstu Biblii...</p>
+                <p style="font-size: 14px;">Jeśli to trwa zbyt długo, sprawdź czy plik biblia-wujka.json jest w tym samym folderze.</p>
             </div>
         `;
         chapterTextDiv.classList.remove("hidden");
@@ -755,9 +336,39 @@ function displayChapterText(chapter) {
         return;
     }
 
+    // Szukaj tekstu w danych
+    const bookData = bibleData[chapter.name];
+    
+    if (!bookData) {
+        chapterTextTitle.textContent = chapterLabel(chapter);
+        chapterTextContent.innerHTML = `
+            <div style="text-align: center; padding: 20px; color: #766f64;">
+                <p>Przepraszamy, nie znaleziono tekstu dla: ${chapter.name}</p>
+            </div>
+        `;
+        chapterTextDiv.classList.remove("hidden");
+        chapterTextDiv.scrollIntoView({ behavior: "smooth" });
+        return;
+    }
+
+    const chapterData = bookData[chapter.chapter];
+
+    if (!chapterData || !chapterData.verses) {
+        chapterTextTitle.textContent = chapterLabel(chapter);
+        chapterTextContent.innerHTML = `
+            <div style="text-align: center; padding: 20px; color: #766f64;">
+                <p>Przepraszamy, nie znaleziono tekstu dla: ${chapter.name}, rozdział ${chapter.chapter}</p>
+            </div>
+        `;
+        chapterTextDiv.classList.remove("hidden");
+        chapterTextDiv.scrollIntoView({ behavior: "smooth" });
+        return;
+    }
+
+    // Wyświetl tekst
     chapterTextTitle.textContent = chapterLabel(chapter);
 
-    const versesHTML = verses.map((verse, index) => {
+    const versesHTML = chapterData.verses.map((verse, index) => {
         return `
             <div class="verse">
                 <span class="verse-number">${index + 1}</span>
@@ -771,7 +382,6 @@ function displayChapterText(chapter) {
     chapterTextDiv.scrollIntoView({ behavior: "smooth" });
 }
 
-
 /*
 ==================================================
 ZAMYKANIE TEKSTU ROZDZIAŁU
@@ -782,7 +392,6 @@ closeChapterButton.addEventListener("click", function() {
     chapterTextDiv.classList.add("hidden");
 });
 
-
 /*
 ==================================================
 URUCHOMIENIE
@@ -790,3 +399,4 @@ URUCHOMIENIE
 */
 
 createPool();
+loadBible(); // Załaduj tekst Biblii przy starcie
